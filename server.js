9eb1b8e895abaf5f947194de7529d0245ee7844d
Matt Closson <mattwclosson@gmail.com>
2021-12-28 14:45:36 -0500

    updated localhost

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const CONFIG = {
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
